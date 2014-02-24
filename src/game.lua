davidyu/mld49

commit cee0b458784304c5c0455ea26aae512e63885d0c

put game over screen in flow
- finish #14

src/game.lua
@@ -5,8 +5,9 @@ require 'socket.http'
 local gui = require 'vendor/Quickie'
 local gamestate = require 'vendor/hump/gamestate'
 local json = require 'vendor/json'
-
 local fonts = {}
+
+-- gamestates
 local game = {}
 game.stats = {}
 game.help = {}
@@ -28,6 +29,15 @@ local function resetlevel()
   camera:setbounds( 0, 0, map.tilewidth * map.width, map.tileheight * map.height )
 
   cmd.init()
+end
+
+local function nextlevel()
+  flow.advance()
+
+  -- check if we're done
+  if table.maxn( flow.seq ) == flow.currentindex() then
+    gamestate.switch( menu.credits )
+  end
 end
 
 function game:keypressed( key )
@@ -132,18 +142,16 @@ function game.stats:update( dt )
   gui.group.push{ grow = "down", pos = { 250, 400 } }
   love.graphics.setFont( fonts["button"] )
   if gui.Button{ text = "advance [spc]", align = "center" } then
-    flow.advance()
-    resetlevel()
-    gamestate.pop()
+    gamestate.pop() -- pop doesn't prevent code from executing
+    nextlevel()
   end
   gui.group.pop()
 end
 
 function game.stats:keypressed( key, code )
   if key == ' ' then
-    flow.advance()
-    resetlevel()
     gamestate.pop()
+    nextlevel()
   end
 end
 
@@ -187,7 +195,6 @@ end
 
 function game:init()
   -- init all modules
-  flow.init()
   cmd.init()
   bot.init()
   doodad.init()
@@ -269,6 +276,10 @@ function game:drawoverlays()
 
     love.graphics.setColor( r, g, b, a )
   end
+end
+
+function game:enter()
+  resetlevel()
 end
 
 function game:draw()

src/flow.lua
@@ -2,6 +2,15 @@ local flow = {}
 
 local utils = require 'utils'
 
+function flow.currentindex()
+  for i, level in ipairs( flow.seq ) do
+    if level == flow.map.name then
+      return i
+    end
+  end
+  return nil
+end
+
 local function nextLevel()
   for i, level in ipairs( flow.seq ) do
     if level == flow.map.name then
@@ -9,6 +18,10 @@ local function nextLevel()
     end
   end
   return nil
+end
+
+function flow.reset()
+  flow.map = utils.buildMap( flow.seq[ 1 ] )
 end
 
 function flow.init()
@@ -28,9 +41,11 @@ function flow.init()
 end
 
 function flow.advance()
-  print( "advancing to: " .. nextLevel():gsub( "art/levels/", "" ) )
   if flow.map.name ~= "art/levels/end" and nextLevel() ~= nil then
     flow.map = utils.buildMap( nextLevel() )
+    return true
+  else
+    return false
   end
 end
 

src/main.lua
@@ -1,9 +1,8 @@
 local gamestate = require 'vendor/hump/gamestate'
 
--- gamestates
-local menu = require 'menu'
-local game = require 'game'
-local credits = {}
+-- public gamestates
+menu = require 'menu'
+game = require 'game'
 
 function love.load()
   if love.window ~= nil then

src/menu.lua
@@ -2,18 +2,18 @@
 local gui = require 'vendor/Quickie'
 local gamestate = require 'vendor/hump/gamestate'
 
--- other states
-local game = require 'game'
+local flow = require 'flow'
 
+-- other states
 local menu = {}
 menu.needname = false
 menu.changename = {}
 menu.changename.inputstr = { text = "" }
-menu.controls = {}
+menu.credits = {}
 local fonts = {}
 
 function menu:init()
-
+  flow.init()
   -- grab saved name, if possible
   bot = game.getbot()
   local namefile = "automaton-mld49"
@@ -55,6 +55,8 @@ function menu.changename:update( dt )
   if gui.Button{ text = "save" } then
     savename( menu.changename.inputstr.text )
     if menu.needname then
+      flow.reset()
+      print( flow.map.name )
       gamestate.switch( game )
       gamestate.push( game.help )
     else
@@ -77,8 +79,61 @@ function menu.changename:keypressed( key, code )
   end
 end
 
+function menu.credits:enter()
+  menu.credits.overlayimg = love.graphics.newImage( "art/overlays/over.png" )
+end
+
+-- Attempts to open a given URL in the system default browser, regardless of Operating System.
+-- pretty awesome snippet from Textmode at http://stackoverflow.com/a/18864453
+local open_cmd -- this needs to stay outside the function, or it'll re-sniff every time...
+function open_url(url)
+  print( string.match( io.popen( "uname -s"):read '*a', "Darwin" ) == "Darwin" )
+  if not open_cmd then
+    if package.config:sub(1,1) == '\\' then -- windows
+      open_cmd = function(url)
+        os.execute(string.format('start "%s"', url))
+      end
+    -- the only systems left should understand uname...
+    elseif string.find( io.popen( "uname -s"):read '*a', "Darwin" ) ~= nil then
+      open_cmd = function(url)
+        os.execute(string.format('open "%s"', url))
+      end
+    else -- that ought to only leave Linux
+      open_cmd = function(url)
+        -- should work on X-based distros.
+        os.execute(string.format('xdg-open "%s"', url))
+      end
+    end
+  end
+
+  open_cmd(url)
+end
+
+function menu.credits:update( dt )
+  gui.group.push{ grow = "right", pos = { 210, 350 } }
+  love.graphics.setFont( fonts["button"] )
+  if gui.Button{ text = "PLAY AGAIN" } then
+    gamestate.switch( menu )
+  end
+  if gui.Button{ text = "SOURCE" } then
+    open_url( "http://github.com/desktop/mld49" )
+  end
+  gui.group.pop{}
+end
+
+function menu.credits:draw()
+  love.graphics.draw( menu.credits.overlayimg )
+  gui.core.draw()
+end
+
+function menu.credits:keypressed( key, code )
+  if key == ' ' or key == 'kpenter' or key == 'return' then
+    gamestate.switch( menu )
+  end
+end
+
 function love.textinput(str)
-    gui.keyboard.textinput(str)
+  gui.keyboard.textinput(str)
 end
 
 function menu:update( dt )
@@ -88,6 +143,7 @@ function menu:update( dt )
     if menu.needname then
       gamestate.push( menu.changename )
     else
+      flow.reset()
       gamestate.switch( game )
     end
   end

commit 183e6fb6389d81487affdf19a7b8fd326b1311fd

add action multiplier overlay
- also fix dead flow (apparently gamestate:enter is not called when you
  pop to it; it's only called when you switch to it)
- finish #13 and fix problem caused by
  cee0b458784304c5c0455ea26aae512e63885d0c

src/game.lua
@@ -144,6 +144,7 @@ function game.stats:update( dt )
   if gui.Button{ text = "advance [spc]", align = "center" } then
     gamestate.pop() -- pop doesn't prevent code from executing
     nextlevel()
+    resetlevel()
   end
   gui.group.pop()
 end
@@ -152,6 +153,7 @@ function game.stats:keypressed( key, code )
   if key == ' ' then
     gamestate.pop()
     nextlevel()
+    resetlevel()
   end
 end
 
@@ -273,6 +275,19 @@ function game:drawoverlays()
     for i, key in ipairs( keys ) do
       love.graphics.print( "< " .. key .. " >", 555, 45 + i * 20 )
     end
+
+    love.graphics.setColor( r, g, b, a )
+  end
+
+  -- action multiplier
+  if cmd.repeatrate > 0 then
+    local r, g, b, a = love.graphics.getColor()
+    love.graphics.setColor( 0, 0, 0, 128 )
+    love.graphics.rectangle( "fill", 5, 5, 200, 28 )
+
+    love.graphics.setColor( 255, 255, 255, 255 )
+    love.graphics.setFont( fonts["button"] )
+    love.graphics.print( "ACTION MULTIPLIER: " .. cmd.repeatrate .. "X", 15, 10 )
 
     love.graphics.setColor( r, g, b, a )
   end